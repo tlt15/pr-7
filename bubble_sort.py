81fd1c1d694588bedef639e8f1f5e35496f5c025
tlt15 <alan.petrovic@mail.ru>
2024-12-18 17:43:08 +0300

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+if __name__ == "__main__":
+    array = [64, 34, 25, 12, 22, 11, 90]
+    print("Original array:", array)
+    sorted_array = bubble_sort(array)
+    print("Sorted array:", sorted_array)
